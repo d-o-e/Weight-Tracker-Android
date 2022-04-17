b1d142ac710e66ddc3afde56596dd8545d711571
denizOzgur <denizozgur@users.noreply.github.com>
2022-04-17 15:27:49 -0700

    init

---
+++ app/src/main/java/com/example/weighttracker/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.weighttracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("ResourceType")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
