e06a5fe223dba992ed9eecab0007258ef788ca61
denizOzgur <denizozgur@users.noreply.github.com>
2022-04-18 18:53:04 -0700

    bump-PC

--- app/src/main/java/com/doe/weighttracker/MainActivity.java
+++ app/src/main/java/com/doe/weighttracker/MainActivity.java
@@ -4,17 +4,40 @@
 package com.doe.weighttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.icu.util.Measure;
+import android.icu.util.MeasureUnit;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editWeight;
+    private EditText editHeight;
+    private Button addData;
+    private Button showHistory;
+
+    WeightHistory history;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WeightHistory history = new WeightHistory(this);
+        history = new WeightHistory(this);
+        addData.setOnClickListener(view -> {
+            // TODO: 4/18/2022 save the data from editTexts above
+        });
+        showHistory.setOnClickListener(view -> {
+            // TODO: 4/18/2022 transition to showHistory activity
+        });
     }
 
-    private double heightInterpret(String height) {
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        history.save();
     }
+
+
 }
--- app/src/main/java/com/doe/weighttracker/WeightHistory.java
+++ app/src/main/java/com/doe/weighttracker/WeightHistory.java
@@ -5,59 +5,118 @@ package com.doe.weighttracker;
 
 import static java.lang.Math.pow;
 import android.content.Context;
-import java.io.File;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class WeightHistory {
+public class WeightHistory extends RecyclerView.Adapter<WeightHistory.WViewHolder> implements Runnable{
     private static final int MAX_DAYS = 365;
-    private ArrayList<WeightData> history;
+    private ArrayList<WeightData> history = new ArrayList<>(MAX_DAYS);
     private Context parent;
     private String fileName;
 
     public WeightHistory(Context context) {
         parent = context;
-        history = new ArrayList<>(MAX_DAYS);
-        fileName = parent.getFilesDir()+"/myWHistory.txt";
-        File fin = new File(fileName);
-        if (fin.exists()) history = readFromFile(fileName);
-        else {
-            try {
-                fin.createNewFile();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        fileName = parent.getFilesDir()+"/myWHistory";
+        run();
+    }
+
+    @Override
+    public void run() {
+        history = readFromFile(fileName);
+        if (history == null) createHistoryFile(fileName);
+
     }
 
     private ArrayList<WeightData> readFromFile(String fileName) {
         ArrayList<WeightData> readFromFile = new ArrayList<>(MAX_DAYS);
-        // TODO: 4/17/22 read lines for size and create indv. weight data
+        FileInputStream fin = null;
+        int index = 0;
         try {
-            FileInputStream fin = new FileInputStream(fileName);
-        } catch (FileNotFoundException e) {
+            fin = new FileInputStream(fileName);
+            // read bytes to history array
+        } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
         return readFromFile;
     }
 
-    class WeightData {
-        private double weight;
-        private double height;
+    public void save() {
+        // TODO: 4/18/2022 auto save on destroy or while suspend
+        /*byte[] bytes = 
+        Files.write((parent.getFilesDir()+fileName), , )*/
+    }
+
+    @NonNull
+    @Override
+    public WViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.historyitem_layout, parent,false);
+        return new WViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WViewHolder holder, int position) {
+        holder.weightHistoryEntryText.setText(history.get(position).toString());
+        /*holder.parent.setOnClickListener(v->{
+            TextView entry = v.findViewById(R.id.weightHistoryEntryTextView);
+        });*/
+    }
+
+    @Override
+    public int getItemCount() {
+        return history.size();
+    }
+
+
+
+    private void createHistoryFile(String fileName) {
+        try {
+            Files.write(Paths.get(fileName), new WeightData(0,new Date()).toString().getBytes(StandardCharsets.UTF_8),StandardOpenOption.CREATE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class WViewHolder extends RecyclerView.ViewHolder {
+        private TextView weightHistoryEntryText;
+        private LinearLayout parent;
+
+        public WViewHolder(@NonNull View itemView) {
+            super(itemView);
+            parent = itemView.findViewById(R.id.linearHistoryLayout);
+            weightHistoryEntryText = itemView.findViewById(R.id.weightHistoryEntryTextView);
+        }
+    }
+
+    private class WeightData {
+        private float weight;
+        private float height;
         private Date date;
-        private double bmi;
+        private float bmi;
         private BMIStatus BMIStatus;
 
-        public WeightData(double weight, Date date) {
+        public WeightData(float weight) {
             this.weight = weight;
-            this.date = date;
-            bmi = (weight /pow(height,2) ) * 703;
+            // TODO: 4/18/2022 init with current time
+            this.date = new Date();
+            bmi = (float) ((weight /pow(height,2) ) * 703.0);
             setBMIStatus();
         }
+
+        // TODO: 4/18/2022 maybe add a private const. for reading from file
 
         private void setBMIStatus(){
             if (bmi<=18.5) BMIStatus = WeightHistory.BMIStatus.UNDER;
@@ -66,6 +125,11 @@ public class WeightHistory {
             else BMIStatus = WeightHistory.BMIStatus.OBESE;
         }
 
+        @NonNull
+        @Override
+        public String toString() {
+            return date + "," + weight + " lbs" + ", bmi=" + bmi +", " + BMIStatus;
+        }
     }
-    enum BMIStatus {UNDER,HEALTHY,OVER,OBESE}
+    private enum BMIStatus {UNDER,HEALTHY,OVER,OBESE}
 }
